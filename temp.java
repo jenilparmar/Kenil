821fcbf4a8a6ce7830a0847eefc4c44cfa7b8387
Jenil <142485028+jenilparmar@users.noreply.github.com>
2026-02-21 20:03:47 +0530

    Fix buggy code

--- temp.java
+++ temp.java
@@ -5,30 +5,35 @@ public class BuggyProgram {
         int[] numbers = {10, 20, 30, 40, 50};
 
         // 1. Off-by-one error (ArrayIndexOutOfBoundsException)
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number: " + numbers[i]);
         }
 
         // 2. NullPointerException
-        String text = null;
-        System.out.println("Text length: " + text.length());
+        String text = "hello";
+        if (text != null) {
+            System.out.println("Text length: " + text.length());
+        }
 
         // 3. Infinite loop (logical bug)
         int count = 0;
         while (count < 5) {
             System.out.println("Count: " + count);
+            count++;
         }
 
         // 4. Division by zero
         int a = 10;
-        int b = 0;
-        System.out.println("Result: " + (a / b));
+        int b = 2;
+        if (b != 0) {
+            System.out.println("Result: " + (a / b));
+        }
 
         // 5. Wrong string comparison (logical bug)
         String s1 = new String("hello");
         String s2 = new String("hello");
 
-        if (s1 == s2) {
+        if (s1.equals(s2)) {
             System.out.println("Strings are equal");
         } else {
             System.out.println("Strings are NOT equal");
