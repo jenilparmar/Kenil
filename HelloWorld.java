a2d7c2a9809dfedd88bb70dde41bd568e46cbc48
Jenil <142485028+jenilparmar@users.noreply.github.com>
2026-03-27 23:05:43 +0530

    Add HelloWorld.java with 50 words of content

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from GitHub!");
+        System.out.println("This is a test Java file created for learning purposes.");
+        System.out.println("Learning Java programming is fun and exciting.");
+        System.out.println("Keep coding every day to improve your skills.");
+        System.out.println("Java is a powerful programming language for enterprise applications.");
+    }
+}
